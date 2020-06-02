103fa43dc7bed3a7f12b8087747977a235a838ac
xinheng.shao <shaoxinheng@gmail.com>
2020-06-02 18:48:37 +0800

    up

--- exercises/ex1.29.lisp
+++ exercises/ex1.29.lisp
@@ -1,21 +1,20 @@
 (load "./examples/c1.3.1-high-order-function.lisp")
-
-(defun cube(n)
-  (* n n n)
-)
+(load "./exercises/libs/math.lisp")
 
 (defun integral(f a b dx)
   (labels 
+    ;; Define the local function
     (
-      (add_dx (x)
-        (+ x dx)
+      (add_dx (x) ; Function's name and parameters.
+        (+ x dx) ; Function's body.
       )
     )
+    ;; Use the local function here
     (* 
       (accumulate 
-        f 
+        f
+        #'add_dx
         (+ a (/ dx 2.0))
-        add_dx
         b
       )
       dx
@@ -23,4 +22,51 @@
   )
 )
 
-(print (integral #'cube 0 1 0.01))
+(defun simpon_integral(f a b n)
+  (let 
+    (
+      (h (/ (- b a) n))
+    )
+    (labels
+      (
+        (y (k)
+          (funcall f (+ a (* k h)))
+        )
+        (current (k)
+          (cond 
+            (
+              (= k 0) (y k)
+            )
+            (
+              (= k n) (y k)
+            )
+            (
+              (isDivided k 2)
+              (* 2 (y k))
+            )
+            ( 
+              (isDivided (+ k 1) 2)
+              (* 4 (y k))
+            )
+          )
+        )
+        ;; (next (k)
+        ;;   (current (+ k 1))
+        ;; )
+      )
+      (*
+        (/ h 3)
+        (accumulate 
+          #'current
+          #'add1
+          a
+          b
+        )
+      )
+    )
+  )
+)
+
+
+(print (integral #'cube 0 1 0.001))
+(print (simpon_integral #'cube 0 1 1000))
