5d379b984d6be4794aae5616d528a3b64845eecf
shaoxinheng <shaoxinheng@gmail.com>
2021-09-09 17:12:27 +0800

    up

--- exercises/ex2.6.lisp
+++ exercises/ex2.6.lisp
@@ -1,8 +1,35 @@
 (load "./exercises/libs/math.lisp")
 
 
-(defun zero(n)
-  (lambda (x) x)
+(defun zero()
+  (lambda (f)
+    (lambda (x) x)
+  )
 )
 
-(print (zero 1))
+(defun add1(n)
+  (lambda (f)
+    (lambda (x) 
+      (funcall f (funcall (funcall n f) x))
+    )
+  )
+)
+
+(defun one()
+  (lambda (f)
+    (lambda (x)
+      (funcall f (funcall f x))
+    )
+  )
+)
+
+(print 
+  (funcall 
+    (funcall 
+      (funcall #'zero) 
+      100
+    )
+    0
+  ) 
+)
+;; (print (add-one #'one))
